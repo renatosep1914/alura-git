9b4d33bc6c0e181d7c263a172017e8031e9f1c0e
renatosep1914 <renatosep1914@gmail.com>
2020-10-30 16:17:50 -0300

    Criando index.html e index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+let celular = function(){
+    
+    this.cor = "prata";
+
+
+}
+
+let objeto = new celular();
+
+console.log(objeto);
+
+
+
+
+// let carros = ["palio", "toro", "uno", 10, true, new Date(), function(){}];
+
+// carros.forEach(function(value, index){
+
+//     console.log(index, value);
+
+// });
+
+// window.addEventListener('focus', event => {
+
+//     console.log("focus");
+
+// });
+
+// document.addEventListener('click', event => {
+
+//     console.log("click");
+    
+// });
+
+
+// function calc(x1, x2, operator){
+
+//     return eval(`${x1} ${operator} ${x2}`);
+
+// }
+// let resultado = calc(1, 2, "-");
+// console.log(resultado);
+
+// let n = 7;
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(`${i} X ${n} = ${i*n}`);
+// }
+
+/*let cor = "azul"
+
+switch (cor){
+    
+    case "verde": 
+        console.log("siga");
+        break;
+    
+    case "amarelo": 
+        console.log("atenção");
+        break;
+
+    case "vermelho": 
+        console.log("pare");
+        break;
+
+    default:
+        console.log("não sei");
+
+}*/
+
+/*let cor = "amarelo";
+
+if (cor === "vermelho"){
+    console.log("siga");
+
+}else if ("amarelo") {
+    console.log("atenção");
+
+}
+else {
+    console.log("pare");
+
+}*/
